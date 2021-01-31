092b5cbc20d83423211b7243fb112b7198f19259
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-31 23:15:43 +0000

    Update src/main/scala/com/grey/sources/DataTypes.scala

--- src/main/scala/com/grey/sources/DataTypes.scala
+++ src/main/scala/com/grey/sources/DataTypes.scala
@@ -2,22 +2,32 @@ package com.grey.sources
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.types.TimestampType
+import org.apache.spark.sql.types.{IntegerType, TimestampType}
 
 class DataTypes() {
 
   def dataTypes(data: DataFrame): DataFrame = {
 
-    // timestamp fields
-    val timestamps = List("start_date", "end_date")
+    // a mutable form of the data frame
     var frame = data
 
+    // timestamp fields
+    val timestamps = List("start_date", "end_date")
     timestamps.foreach { field =>
       val temporary = s"temp_$field"
       frame = frame.withColumn(temporary, col(field).cast(TimestampType)).drop(field)
         .withColumnRenamed(temporary, field)
 
     }
+
+    // integer fields
+    val integers = List("duration", "start_station_number", "end_station_number")
+    integers.foreach{field =>
+      val temporary = s"temp_$field"
+      frame = frame.withColumn(temporary, col(field).cast(IntegerType)).drop(field)
+        .withColumnRenamed(temporary, field)
+    }
+
     frame
 
   }
