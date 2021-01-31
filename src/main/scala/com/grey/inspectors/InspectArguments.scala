b268cd80084e2218443056d889d8d5d9cb3b986c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-31 17:47:30 +0000

    Add src/main/scala/com/grey/inspectors/InspectArguments.scala: Verifies the ...
    ... contents of the package arguemnt.  The package argument is a URL to a YAML file.

---
+++ src/main/scala/com/grey/inspectors/InspectArguments.scala
@@ -0,0 +1,56 @@
+package com.grey.inspectors
+
+import java.net.URL
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+
+import scala.util.Try
+import scala.util.control.Exception
+
+object InspectArguments {
+
+  def inspectArguments(args: Array[String]): Parameters = {
+
+    // Is the string args(0) a URL?
+    val isURL: Try[Boolean] = new IsURL().isURL(args(0))
+
+    // If the string is a valid URL parse & verify its parameters
+    val mapper: ObjectMapper = new ObjectMapper(new YAMLFactory())
+
+    val getParameters: Try[Parameters] = if (isURL.isSuccess){
+      Exception.allCatch.withTry(
+        mapper.readValue(new URL(args(0)), classOf[Parameters])
+      )
+    } else {
+      sys.error(isURL.failed.get.getMessage)
+    }
+
+    if (getParameters.isSuccess) {
+      getParameters.get
+    } else {
+      sys.error(getParameters.failed.get.getMessage)
+    }
+
+  }
+
+  // Verification of parameters
+  class Parameters(@JsonProperty("dataSet") _dataSet: List[String],
+                   @JsonProperty("typeOf") _typeOf: String,
+                   @JsonProperty("schemaOf") _schemaOf: List[String]){
+
+    require(_dataSet != null, "The parameter dataSet, i.e., the list of folders that lead to the data set of " +
+      "interest is required.  The list items must be sequentially relative to the resources directory.")
+    val dataSet: List[String] = _dataSet
+
+    require(_typeOf != null, "The parameter typeOf, i.e., the extension string of the files, is required.")
+    val typeOf: String = _typeOf
+
+    require(_schemaOf != null, "The parameter schemaOf, i.e., the list of folders + schema file name, " +
+      "is required.  The list items must be sequentially relative to the resources directory.")
+    val schemaOf: List[String] = _schemaOf
+
+  }
+
+}
