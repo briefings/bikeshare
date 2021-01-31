a7c4eea4f2f39092c56d8436c3efc5d6f7450ec4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-31 22:42:35 +0000

    Update src/main/scala/com/grey/DataSteps.scala

--- src/main/scala/com/grey/DataSteps.scala
+++ src/main/scala/com/grey/DataSteps.scala
@@ -2,14 +2,23 @@ package com.grey
 
 import com.grey.inspectors.InspectArguments
 import com.grey.sources.DataRead
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 class DataSteps(spark: SparkSession) {
 
   def dataSteps(parameters: InspectArguments.Parameters): Unit = {
 
-    val frame: DataFrame = new DataRead(spark = spark).dataRead(parameters = parameters)
-    frame.show()
+    val (ridesFrame: DataFrame, ridesSet: Dataset[Row]) = new DataRead(spark = spark)
+      .dataRead(parameters = parameters)
+
+    println("\nFrame")
+    ridesFrame.show(5)
+    ridesFrame.printSchema()
+
+    println("\nAnd")
+    ridesSet.show(5)
+    ridesSet.printSchema()
+
 
   }
 
